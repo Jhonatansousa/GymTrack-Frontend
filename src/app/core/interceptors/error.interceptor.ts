ea2c41ad9f85068194eb33678f3105b369b9d576
Jhonatan <jhonatandev@outlook.com>
2026-04-25 18:26:44 -0300

    test(error-interceptor): validate real behaviour in green phase

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,3 +1,17 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
-export const errorInterceptor: HttpInterceptorFn = (req, next) => next(req);
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        void router.navigate(['/auth'], { replaceUrl: true });
+      }
+      return throwError(() => error);
+    }),
+  );
+};
